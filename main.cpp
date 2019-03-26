c0c72706a54bda3588497fc75519fdc861f36c88
Andy Roxby <aroxby@users.noreply.github.com>
2019-03-26 19:31:43 -0400

    Link with SDL2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "Hello CMake\n";
+    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr){
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Delay(5000);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
     return 0;
 }
