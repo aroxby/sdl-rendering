e5826c050008abd26470f7fd7a9bc6a2b0647541
Andy Roxby <aroxby@users.noreply.github.com>
2019-03-26 20:04:43 -0400

    Create basic draw and event loop

--- main.cpp
+++ main.cpp
@@ -2,15 +2,46 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
+void draw(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawLine(renderer, 320, 200, 300, 240);
+    SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
+    SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
+    SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char *argv[]) {
-    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == nullptr){
-        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    if(SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer)) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
-    SDL_Delay(5000);
-    SDL_DestroyWindow(win);
+
+    bool done = false;
+    SDL_Event event;
+    do {
+        draw(renderer);
+
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                done = true;
+            }
+        }
+    }while(!done);
+
+    if (renderer) {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window) {
+        SDL_DestroyWindow(window);
+    }
     SDL_Quit();
+
     return 0;
 }
