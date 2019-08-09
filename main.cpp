90a448dd62f79902fa854f0fe4d4eb645635260a
Andy Roxby <aroxby@users.noreply.github.com>
2019-08-08 20:10:43 -0400

    Attempt to play sound as well

--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
+class WavSound {
+public:
+    SDL_AudioSpec wavSpec;
+    Uint32 wavLength;
+    Uint8 *wavBuffer;
+
+    WavSound(const char *path) {
+        SDL_LoadWAV(path, &wavSpec, &wavBuffer, &wavLength);
+    }
+    
+    ~WavSound() {
+        SDL_FreeWAV(wavBuffer);
+    }
+
+    int play(SDL_AudioDeviceID audioDevice) {
+        int err = SDL_QueueAudio(audioDevice, wavBuffer, wavLength);
+        if(!err) {
+            SDL_PauseAudioDevice(audioDevice, 0);
+        }
+        return err;
+    }
+};
+
 void draw(SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
@@ -17,11 +40,17 @@ int main(int argc, char *argv[]) {
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
 
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+
     if(SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer)) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
+
+    WavSound sound("../Blackbird.SoundBible.wav");
+    SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(NULL, 0, &sound.wavSpec, NULL, 0);
+    sound.play(audioDevice);
 
     bool done = false;
     SDL_Event event;
@@ -41,6 +70,8 @@ int main(int argc, char *argv[]) {
     if (window) {
         SDL_DestroyWindow(window);
     }
+
+    SDL_CloseAudioDevice(audioDevice);
     SDL_Quit();
 
     return 0;
